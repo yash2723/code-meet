01d96d5cf8c0bdb5fdb00530ac03b0b3cb94e489
Yash Upadhyay <yash.upadhyay.2723@gmail.com>
2022-10-26 18:52:30 +0530

    Socket URL modified

--- src/socket.js
+++ src/socket.js
@@ -7,5 +7,5 @@ export const initSocket = async () => {
         timeout : 10000,
         transports : ['websocket'],
     };
-    return io("http://localhost:9885/" , options);
+    return io("http://code-meet-27.herokuapp.com/" , options);
 }
